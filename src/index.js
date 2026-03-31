bd7cbd039c8d9e2db1134d040df5caba28f25918
AntoineGallou31 <antoinegallou@hotmail.fr>
2026-03-31 10:48:34 +0200

    cors config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ const mealPlanRouter = require('./routes/mealPlan');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://meal-app.vercel.app'
+  ]
+}));
 app.use(express.json());
 
 app.use('/api/recipes', recipesRouter);
